04f34fc02e2514ac4a6915cd90494447dbf8dea8
gizmore <gizmore@wechall.net>
2017-10-14 22:53:02 +0200

    6.05 layout

--- GDO/UI/tpl/cell/link.php
+++ GDO/UI/tpl/cell/link.php
@@ -1,2 +1,2 @@
 <?php /** @var $link \GDO\UI\GDT_Link **/ ?>
-<a class="gdt-link" href="<?= $link->href; ?>"><?= $link->label; ?>&nbsp;<?= $link->htmlIcon(); ?></a>
+<a class="gdt-link" href="<?= $link->href; ?>"><?= $link->label; ?><?= $link->htmlIcon(); ?></a>
