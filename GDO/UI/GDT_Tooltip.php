2eb21a553cd9458ea820a7747b61f396e45334e9
gizmore <gizmore@wechall.net>
2017-10-17 00:31:16 +0200

    6.06_fixes

--- GDO/UI/GDT_Tooltip.php
+++ GDO/UI/GDT_Tooltip.php
@@ -6,6 +6,11 @@ use GDO\Core\GDT_Template;
 class GDT_Tooltip extends GDT_Icon
 {
     use WithTooltip;
+    
+    public function __construct()
+    {
+    	$this->icon('help');
+    }
 
 	public function renderCell() { return GDT_Template::php('UI', 'cell/tooltip.php', ['field'=>$this]); }
 }
