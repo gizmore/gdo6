fd1618aebd3fdffb38137dfec03d5574830d7bc8
gizmore <gizmore@wechall.net>
2018-02-04 23:36:37 +0100

    6.07_classic_fixes

--- GDO/UI/tpl/cell/button.php
+++ GDO/UI/tpl/cell/button.php
@@ -3,7 +3,7 @@ use GDO\UI\GDT_Button;
 $field instanceof GDT_Button;
 ?>
 <?php if ($href) : ?>
-<a class="md-button md-primary md-raised" href="<?= $href; ?>" <?= $field->htmlDisabled(); ?>>
+<a class="gdo-button" href="<?= $href; ?>" <?= $field->htmlDisabled(); ?>>
   <?= $field->displayLabel(); ?>
   <?= $field->htmlIcon(); ?>
 </a>
--- GDO/UI/tpl/form/button.php
+++ GDO/UI/tpl/form/button.php
@@ -1,11 +1,10 @@
 <?php
 use GDO\UI\GDT_Button;
-
 $field instanceof GDT_Button;
 ?>
-<a class="md-button md-secondary gdo-button"
+<a class="gdo-button"
  <?= $field->htmlDisabled(); ?>
  <?= $field->htmlHREF(); ?>>
-  <?= $field->htmlIcon(); ?>
-  <?= $field->displayLabel(); ?>
+ <?= $field->htmlIcon(); ?>
+ <?= $field->displayLabel(); ?>
 </a>
