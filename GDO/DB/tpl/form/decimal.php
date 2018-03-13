195910393bfce3267204be164dc57f3f21925afc
gizmore <gizmore@wechall.net>
2018-03-13 20:41:02 +0100

    6.07_Various

---
+++ GDO/DB/tpl/form/decimal.php
@@ -0,0 +1,15 @@
+<?php /** @var $field \GDO\DB\GDT_Decimal **/ ?>
+<div class="gdo-container<?= $field->classError(); ?>">
+  <?= $field->htmlIcon(); ?>
+  <label for="form[<?= $field->name; ?>]"><?= $field->label; ?></label>
+  <input
+   type="number"
+   name="form[<?= $field->name; ?>]"
+   <?= $field->htmlDisabled(); ?>
+   <?= $field->htmlRequired(); ?>
+   min="<?= $field->min; ?>"
+   max="<?= $field->max; ?>"
+   step="<?= $field->step; ?>"
+   value="<?= $field->getVar(); ?>" />
+  <?= $field->htmlError(); ?>
+</div>
