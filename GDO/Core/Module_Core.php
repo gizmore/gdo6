4a41f79d539e4b50eee365924845f4c138e771ab
gizmore <gizmore@wechall.net>
2018-04-12 22:03:11 +0200

    6.07_classic_menu

--- GDO/Core/Module_Core.php
+++ GDO/Core/Module_Core.php
@@ -77,8 +77,9 @@ final class Module_Core extends GDO_Module
     ##################
     public function onIncludeScripts()
     {
-        $this->addCSS('css/gdo6.css');
-        $this->addJavascript('js/gdo-string-util.js');
+    	$this->addCSS('css/gdo6.css');
+    	$this->addCSS('css/gdo6-sidebar.css');
+    	$this->addJavascript('js/gdo-string-util.js');
         $this->addJavascript('js/gdo-user.js');
         $this->addJavascript('js/gdo-core.js');
         Javascript::addJavascriptInline($this->gdoConfigJS());
