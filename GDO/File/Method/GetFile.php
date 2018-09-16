680749227b4d01446c3c19ae45950bec7389cf71
gizmore <gizmore@wechall.net>
2018-09-16 21:26:34 +0200

    6.08-r39

--- GDO/File/Method/GetFile.php
+++ GDO/File/Method/GetFile.php
@@ -50,7 +50,7 @@ final class GetFile extends Method
 		{
 			return $this->error('err_file_not_found', [htmlspecialchars($path)]);
 		}
-		Stream::serve($file, $variant);
+		Stream::serve($file, $variant, (!isset($_GET['nodisposition'])));
 	}
 	
 }
