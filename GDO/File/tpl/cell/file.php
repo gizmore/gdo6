880e7ede15ddab102a6eac5710f570e42bb90aac
gizmore <gizmore@wechall.net>
2019-03-09 00:38:40 +0100

    6.09-r81_fixes

--- GDO/File/tpl/cell/file.php
+++ GDO/File/tpl/cell/file.php
@@ -1,10 +1,16 @@
 <?php /** @var $gdo \GDO\File\GDO_File **/
 use GDO\UI\GDT_Icon; ?>
+<?php if (!$gdo) return; ?>
 <div class="gdo-file">
-<?php if ($gdo) : ?>
+<?php if ($gdo->isImageType()) : ?>
+<img
+ style="display: block;"
+ src="<?= href('File', 'GetFile', '&file='.$gdo->getID()); ?>" />
+<?php else : ?>
   <?= GDT_Icon::iconS('file'); ?>
+<?php endif; ?>
+  <span class="gdo-file-title"><?= $gdo->displayName(); ?></span>
   <span class="gdo-file-size"><?= $gdo->displaySize(); ?></span>
   <span class="gdo-file-type"><?= $gdo->getType(); ?></span>
   <div class="cf"></div>
-<?php endif; ?>
 </div>
