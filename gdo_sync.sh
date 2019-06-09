ac0665d950478c1935ebca81eb41f376f6bb199c
gizmore <gizmore@wechall.net>
2019-06-09 02:44:04 +0200

    6-09-r602_sync

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -4,7 +4,4 @@ cd "$(dirname "$0")"
 CORE="$(dirname "$0")"
 
 echo "git commit all repos"
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am '$1'" \;
-
-echo "git push all repos"
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git push" \;
+find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am '$1' && git push" \;
