fe0e61f84aaa82e2823d283a948436f3a37ac650
alex <alex@wechall.net>
2019-06-14 18:35:59 +0200

    6.09_update_sleep

--- gdo_update.sh
+++ gdo_update.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 
 CORE="$(dirname "$0")"
 
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && LANG=en_GB LC_ALL=en_GB git pull  && git submodule update --recursive --remote && sleep 1" \;
+find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && LANG=en_GB LC_ALL=en_GB git pull && sleep 3 && git submodule update --recursive --remote && sleep 3" \;
