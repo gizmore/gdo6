91a6ec0e391326bebc4f2d3b7b9bdcd9831feae2
gizmore <gizmore@wechall.net>
2019-12-16 19:48:09 +0100

    6.10-r1043_de_lang

--- GDO/Language/Method/GetTranslationData.php
+++ GDO/Language/Method/GetTranslationData.php
@@ -2,10 +2,8 @@
 namespace GDO\Language\Method;
 use GDO\Core\MethodAjax;
 use GDO\Language\Trans;
-use GDO\Core\Website;
 use GDO\Core\GDT_Response;
 use GDO\Core\GDT_JSON;
-use GDO\Core\Method;
 
 final class GetTranslationData extends MethodAjax
 {
