d267a53f8a26ebfcff17acf4c8eb672e4b6c0398
gizmore <gizmore@wechall.net>
2020-02-07 10:50:47 +0100

    6.10-r5012_licenses

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 CORE="$(dirname "$0")"
 
 echo "git commit all repos"
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git diff-index --quiet HEAD || git commit -am '$1' && test "$(git status --porcelain)" || git push" \;
+find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && (git diff-index --quiet HEAD || git commit -am '$1') && (test \"$(git status --porcelain)\" || git push)" \;
