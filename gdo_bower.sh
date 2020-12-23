5d6fca15c84afb9c48e81ef0f60382f8009acf00
gizmore <gizmore@wechall.net>
2020-12-23 10:54:26 +0100

    Force bower update

--- gdo_bower.sh
+++ gdo_bower.sh
@@ -5,4 +5,4 @@ CORE="$(dirname "$0")"
 
 echo "All modules: bower update"
 echo "Thanks to greycat@freenode#bash"
-for d in GDO/*/; do (cd "$d" || exit; [[ -f bower.json ]] || exit; echo $d; bower update); done
+for d in GDO/*/; do (cd "$d" || exit; [[ -f bower.json ]] || exit; echo $d; bower --force update); done
