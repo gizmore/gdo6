507779801b93714c92ad2162f3de6a078713621d
livinskull <livinskull@gmail.com>
2021-01-14 22:01:09 +0100

    Add missing space to version check output

--- GDO/Install/lang/install_en.php
+++ GDO/Install/lang/install_en.php
@@ -4,11 +4,11 @@ return array(
 'install_title_1' => 'Welcome',
 'install_text_1' => 'Welcome to GDO6, Please continue here: %s',
 'install_text_2' => 'If you plan to use a Database, please execute the following mysql commands.',
-    
+
 # System Test
 'install_title_2' => 'System–Test',
 'install_title_2_tests' => 'Mandatory Requirements',
-'install_test_0' => 'Is PHP Version '.PHP_VERSION.'supported?',
+'install_test_0' => 'Is PHP Version '.PHP_VERSION.' supported?',
 'install_test_1' => 'Is the protected folder writable?',
 'install_test_2' => 'Is the files folder writable?',
 'install_test_3' => 'Is the temp folder writable?',
