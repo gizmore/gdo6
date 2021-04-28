84a615e9360deebc2e8e8113cad8bdfd162d1ab9
flederohr <30510478+flederohr@users.noreply.github.com>
2021-04-29 01:49:52 +0200

    Update gdo_update.sh
    Implements parallel execution via xargs -P $THREADS.
    The number of simultanious processes can be specified as a cmdline argument.

--- gdo_update.sh
+++ gdo_update.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")"
 
-CORE="$(dirname "$0")"
-
-SLEEP=0
+# THREADS: number of parallel executing  processes
+# default: THREADS=0 (unlimited)
+THREADS=0
 if [ $# -gt 0 ]; then
-  SLEEP=$1
+  THREADS=$1
 fi;
 
-echo "Updating all repos with a sleep of $SLEEP seconds."
+XARGS_OPTIONS="-P $THREADS -0 -I {}"
+if [ $(uname -s) == "FreeBSD" ]; then
+        XARGS_OPTIONS="-S 1024 -R $XARGS_OPTIONS"
+fi
 
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && 
-pwd && LANG=en_GB LC_ALL=en_GB git pull && sleep $SLEEP && git submodule update --recursive --remote && sleep $SLEEP" \;
+echo "Updating all repos in parallel threads."
+
+find . -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> /tmp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> /tmp/git_pull_\$OUT && git submodule update --recursive --remote &>> /tmp/git_pull_\$OUT  && cat /tmp/git_pull_\$OUT && rm /tmp/git_pull_\$OUT "
