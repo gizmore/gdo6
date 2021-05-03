42faac2a0ef6c23e4c41277dc3c934ec6752798e
gizmore <gizmore@wechall.net>
2021-05-03 11:19:58 +0200

    6.10.1-r1062_table_ordername_prediction

--- GDO/Table/Sort.php
+++ GDO/Table/Sort.php
@@ -34,6 +34,7 @@ final class Sort
         # Create a table to sort with
         $table = GDT_Table::make('sort_table');
         $table->addHeaders($result->table->gdoColumnsCache());
+        $table->headers->name = '_mosort_';
         
         # Plug orders into request vars
         $o = $table->headers->name;
@@ -47,6 +48,7 @@ final class Sort
         $table->multisort($result);
 
         # Ugly: fix order name prediction
+        unset($_REQUEST[$o]);
         GDT_Table::$ORDER_NAME--;
     }
     
