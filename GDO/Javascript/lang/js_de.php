e1bef862c39a2d07c149b43db77bb9544c39b2b9
gizmore <gizmore@wechall.net>
2021-05-03 03:38:14 +0200

    6.10.1-r1053_fixesnew_js_module

---
+++ GDO/Javascript/lang/js_de.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'cfg_minify_js' => 'Javascript Minifizierungs-Modus',
+    'cfg_nodejs_path' => 'Pfad zu nodejs',
+    'cfg_uglifyjs_path' => 'Pfad zu uglifyjs',
+    'cfg_ng_annotate_path' => 'Pfad zu ng-annotate',
+    'cfg_link_node_detect' => 'Javascript Programme auf Server suchen',
+    'link_node_detect' => 'Detect javascript binaries…',
+    'msg_nodejs_detected' => 'The nodejs binary has been detected: <i>%s</i>',
+    'msg_annotate_detected' => 'The ng-annotate binary has been detected: <i>%s</i>',
+    'msg_uglify_detected' => 'The uglify-js binary has been detected: <i>%s</i>',
+    'err_nodejs_not_found' => 'Could not find nodejs',
+    'err_annotate_not_found' => 'Could not find ng-annotate',
+    'err_uglify_not_found' => 'Could not find uglify',
+    'minify_js' => 'Javascript optimieren?',
+    'enum_concat' => 'auch zusammenführen',
+    'info_detect_node_js' => 'Detect installed js binaries.<br/>Install them <em>before</em> you run this method.<br/><br/>apt-get install nodejs<br/>npm install uglify-js -g<br/>npm install -g ng-annotate-patched<br/>',
+];
