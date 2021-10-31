97b292b8ce9b79a23623415de301f98ffd0f8d0e
gizmore <gizmore@wechall.net>
2021-10-31 02:03:17 +0100

    6.10.6-r6149

--- GDO/Core/Module_Core.php
+++ GDO/Core/Module_Core.php
@@ -38,7 +38,7 @@ final class Module_Core extends GDO_Module
      * 
      * @var string
      */
-	const GDO_REVISION = '6.10.6-r6148';
+	const GDO_REVISION = '6.10.6-r6149';
 
 	##############
 	### Module ###
