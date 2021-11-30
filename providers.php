2839e951ea54cc3128910cd4484786565a98a2ec
gizmore <gizmore@wechall.net>
2021-11-30 01:45:56 +0100

    6.11.0-r6218

--- providers.php
+++ providers.php
@@ -3,9 +3,9 @@
  * This prints all modules and their providers.
  * The list can be copied by gdo6 authors to Core/ModuleProviders.php
  */
-
 use GDO\File\Filewalker;
 use GDO\Util\Common;
+use GDO\Util\Strings;
 
 # Use gdo6 core
 include "GDO6.php";
@@ -14,23 +14,35 @@ include "protected/config.php";
 global $mode;
 
 /** @var $argv string **/
-$mode = $argv[1];
+$mode = @$argv[1];
 
-Filewalker::traverse("GDO", null, false, function($entry, $fullpath) {
-   if (is_dir('GDO/'.$entry."/.git"))
-   {
-       global $mode;
-       $c = file_get_contents('GDO/'.$entry."/.git/config");
-       $c = Common::regex('#/gizmore/([-_a-z0-9]+)#m', $c);
-       if ($mode)
-       {
-           echo "$entry - <https://github.com/gizmore/$c>\n\n";
-       }
-       else
-       {
-           echo "'".$entry."' => '$c',\n";
-       }
-   }
-},  0);
+if ($mode)
+{
+	echo "'Captcha' => ['gdo6-captcha', 'gdo6-recaptcha2'],\n";
+	echo "'Session' => ['gdo6-session-db', 'gdo6-session-cookie'],\n";
+}
+
+Filewalker::traverse("GDO", null, false,
+function ($entry, $fullpath)
+{
+	if (is_dir('GDO/' . $entry . "/.git"))
+	{
+		global $mode;
+		$c = file_get_contents('GDO/' . $entry . "/.git/config");
+		$c = Common::regex('#/gizmore/([-_a-z0-9]+)#m', $c);
+		if (Strings::startsWith($entry, 'gdo6-'))
+		{
+			return;
+		}
+		if (!$mode)
+		{
+			echo "$entry - < https://github.com/gizmore/$c >\n";
+		}
+		else
+		{
+			echo "'" . $entry . "' => '$c',\n";
+		}
+	}
+}, 0);
 
 
