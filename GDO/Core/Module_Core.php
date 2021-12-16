2880f133640c99f367761933602017c2c3719568
gizmore <gizmore@wechall.net>
2021-12-16 15:18:26 +0100

    6.11.1-r6260-marco-madness

--- GDO/Core/Module_Core.php
+++ GDO/Core/Module_Core.php
@@ -39,7 +39,7 @@ final class Module_Core extends GDO_Module
      * 
      * @var string
      */
-	const GDO_REVISION = '6.11.1-r6259';
+	const GDO_REVISION = '6.11.1-r6260';
 
 	##############
 	### Module ###
