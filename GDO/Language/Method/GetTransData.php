c2dc0d4dfa1fccd2a994d70c5e4051aac4e5ac4f
gizmore <gizmore@wechall.net>
2022-02-18 02:30:46 +0100

    6.11.3-r6319

--- GDO/Language/Method/GetTransData.php
+++ GDO/Language/Method/GetTransData.php
@@ -4,6 +4,7 @@ namespace GDO\Language\Method;
 use GDO\Core\MethodAjax;
 use GDO\Language\Trans;
 use GDO\Core\Application;
+use GDO\Core\GDT_Array;
 
 /**
  * Get all translation data for the current language.
@@ -17,11 +18,20 @@ final class GetTransData extends MethodAjax
 {
 	public function execute()
 	{
-	    $langdata = json_encode(Trans::getCache(Trans::$ISO), JSON_PRETTY_PRINT);
+		# Get data
+		$trans = Trans::getCache(Trans::$ISO);
+		
+		# 
+		if (Application::instance()->isJSON())
+		{
+			return GDT_Array::makeWith($trans);
+		}
+		
+	    $langdata = json_encode($trans, JSON_PRETTY_PRINT);
 	    $code = sprintf('window.GDO_TRANS = {}; window.GDO_TRANS.CACHE = %s;', $langdata);
 	    if (!Application::instance()->isUnitTests())
 	    {
-	        hdr('Content-Type: application/javascript');
+	        hdr('Content-Type: text/javascript');
 	        die($code);
 	    }
 	}
